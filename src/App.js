95d38aa18bf829358d8d498f974e6eead9b1989b
Vanitha <vanithapg@gmail.com>
2021-06-14 00:06:30 -0500

    test and dockerized

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import { Link, Route, BrowserRouter as Router } from "react-router-dom";
-import React, { useState} from 'react'
+import React, { useState } from "react";
 
-
-import './App.css';
-import FoodItems from './FoodItems'
-import Recipe from './Recipe'
-import Food from './Food'
+import "./App.css";
+import FoodItems from "./FoodItems";
+import Recipe from "./Recipe";
+import Food from "./Food";
 
 function App() {
-
-  const [foodItems, setFoodItems ] = useState([]);
+  // const [foodItems, setFoodItems ] = useState([]);
 
   // useEffect(() => {
   //     fetch("http://localhost:6800/food", { method: "GET" })
@@ -27,10 +25,10 @@ function App() {
       <h1> Food Blog</h1>
       <hr />
       <Router>
-        <Route exact path="/" component={FoodItems}/>
+        <Route exact path="/" component={FoodItems} />
         <Route path="/recipe/:id" component={Recipe}></Route>
         <Route path="/food/:id">
-          <Food/>
+          <Food />
         </Route>
       </Router>
     </div>
